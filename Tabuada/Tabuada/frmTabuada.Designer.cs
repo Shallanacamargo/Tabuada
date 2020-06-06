580058dca6474f2fd57681e1fc9382f0ca57a4c7
pedronet5 <pedronet5@gmail.com>
2020-06-06 16:36:12 -0300

    first commit

---
+++ Tabuada/Tabuada/frmTabuada.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Tabuada
+{
+    partial class frmTabuada
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ltbResultado = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(6, 20);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(73, 20);
+            this.txtValor.TabIndex = 0;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnCalcular.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(85, 19);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(153, 22);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "CALCULAR";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ltbResultado);
+            this.groupBox1.Controls.Add(this.txtValor);
+            this.groupBox1.Controls.Add(this.btnCalcular);
+            this.groupBox1.Location = new System.Drawing.Point(3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(252, 287);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // ltbResultado
+            // 
+            this.ltbResultado.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ltbResultado.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ltbResultado.FormattingEnabled = true;
+            this.ltbResultado.ItemHeight = 16;
+            this.ltbResultado.Location = new System.Drawing.Point(6, 42);
+            this.ltbResultado.Name = "ltbResultado";
+            this.ltbResultado.Size = new System.Drawing.Size(232, 228);
+            this.ltbResultado.TabIndex = 2;
+            // 
+            // frmTabuada
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(267, 291);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmTabuada";
+            this.Text = "Tabuada";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox ltbResultado;
+    }
+}
+
---
+++ Tabuada/Tabuada/frmTabuada.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Tabuada
+{
+    public partial class frmTabuada : Form
+    {
+        public frmTabuada()
+        {
+            InitializeComponent();
+        }
+
+        //Ação do botão calcular
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (Validar())
+            {
+                //limpando o listbox 
+                ltbResultado.Items.Clear();
+
+                //Convertendo string para int
+                int valor = Convert.ToInt32(txtValor.Text);
+
+                for (int i = 0; i <= 10 ; i++)
+                {
+                    //Realizando a multiplicação
+                    int resultado = valor * i;
+
+                    // Preenchimento do listbox
+                    ltbResultado.Items.Add(valor + "x" + i + "=" + resultado);
+                }
+            }
+        }
+
+        private bool Validar()
+        {
+            bool retorno = true;
+
+            //Validação se o valor informado e númerico.
+            if (string.IsNullOrEmpty(txtValor.Text) || !IsNumeric(txtValor.Text))
+            {
+                retorno = false;
+                MessageBox.Show("Digíte um Valor Númerico.", MessageBoxIcon.Information.ToString());
+            }
+
+            return retorno;
+        }
+
+        /// <summary>
+        /// Verificação se o valor e númerico
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool IsNumeric(string value)
+        {
+            return value.All(char.IsNumber);
+        }
+    }
+}
